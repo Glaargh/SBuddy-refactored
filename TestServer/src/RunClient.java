aa7f7b962458cb5bc08c43f98a745d10aea6a6d5
Michael Tran <M.Tran@student.tudelft.nl>
2015-11-25 16:57:13 +0100

    Created a basic server-client application

---
+++ TestServer/src/RunClient.java
@@ -0,0 +1,9 @@
+
+public class RunClient {
+	public static void main(String[] args){
+		Client socketFrame2 = new Client();
+		socketFrame2.setVisible(true);
+		socketFrame2.startClient();
+		
+	}
+}
