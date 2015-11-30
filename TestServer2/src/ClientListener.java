b1d049c74d76d81080cb7d899e6f753183c52af4
Michael Tran <M.Tran@student.tudelft.nl>
2015-11-30 15:09:04 +0100

    ghfh

---
+++ TestServer2/src/ClientListener.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+public class ClientListener implements Runnable {
+	private Socket connection;
+	PrintWriter out;
+    BufferedReader in;
+    
+    public ClientListener(Socket connectionIn){
+    	this.connection = connectionIn;
+    	
+    }
+    
+    public void run()
+    {
+        try{
+           String message;
+            
+            //3. get Input and Output streams
+            out = new PrintWriter(connection.getOutputStream(), true);
+            //out.flush();
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            sendMessage("Connection successful");
+            
+            //4. The two parts communicate via the input and output streams
+            do{
+                message = (String)in.readLine();
+				System.out.println("client>" + message);
+				sendMessage(message);
+				if (message.equals("bye"))
+				    sendMessage("bye");
+            }while(!message.equals("bye"));
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                connection.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+    void sendMessage(String msg)
+    {
+        out.println(msg);
+		out.flush();
+
+    }
+}
--- TestServer2/src/Provider.java
+++ TestServer2/src/Provider.java
@@ -3,15 +3,13 @@ import java.net.*;
 public class Provider{
     ServerSocket providerSocket;
     Socket connection = null;
-    PrintWriter out;
-    BufferedReader in;
-    String message;
+    private ClientListener listener;
     Provider(){}
     void run()
     {
         try{
             //1. creating a server socket
-            providerSocket = new ServerSocket(2055, 10);
+            providerSocket = new ServerSocket(2055);
             //2. Wait for connection
             System.out.println("Waiting for connection");
             connection = providerSocket.accept();
@@ -19,41 +17,17 @@ public class Provider{
             System.out.println("Connected to " + connection.getInetAddress() + " on port "
 			          + connection.getPort() + " FROM port " + connection.getLocalPort() + " of "
 			          + connection.getLocalAddress());
-            //3. get Input and Output streams
-            out = new PrintWriter(connection.getOutputStream(), true);
-            out.flush();
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            sendMessage("Connection successful");
-            //4. The two parts communicate via the input and output streams
-            do{
-                message = (String)in.readLine();
-				System.out.println("client>" + message);
-				sendMessage(message);
-				if (message.equals("bye"))
-				    sendMessage("bye");
-            }while(!message.equals("bye"));
+            
+            //3. Creating a thread for incoming client
+            listener = new ClientListener(connection);
+            Thread t = new Thread(listener);
+            t.start();
         }
         catch(IOException ioException){
             ioException.printStackTrace();
         }
-        finally{
-            //4: Closing connection
-            try{
-                in.close();
-                out.close();
-                providerSocket.close();
-            }
-            catch(IOException ioException){
-                ioException.printStackTrace();
-            }
-        }
     }
-    void sendMessage(String msg)
-    {
-        out.println(msg);
-		out.flush();
-
-    }
+    
     public static void main(String args[])
     {
         Provider server = new Provider();
