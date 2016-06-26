fecc6d9d1011d9cf4ad25e364a494153bb9ced9c
Dario Hartog <dhartog@student.tudelft.nl>
2016-06-26 22:10:56 +0200

    Started testing the Parser class.

---
+++ SBuddy/src/ParserTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testParser() {
+		Parser testParser = new Parser();
+		assertNotNull(testParser);
+	}
+
+	@Test
+	public void testParse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseCourses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseAvailability() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseUserInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseMatchedEmails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseMatches() {
+		fail("Not yet implemented");
+	}
+
+}
